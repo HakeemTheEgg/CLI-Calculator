ae63454ff0ca35704761dd49673297fc749e2f61
Abdulhakeem <abdul.d.blendamasta@gmail.com>
2026-01-14 23:12:20 +0100

    basic cli tool

--- rust_calculator/src/main.rs
+++ rust_calculator/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::io;
+
+fn main(){
+    println!("rust calculator");
+    println!("Enter an expression: ");
+    println!("type exit to quit");
+
+    
+    loop{
+
+        let mut input = String::new();
+        println!("> ");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input = input.trim();
+        if input == "exit"{
+            break;
+        }
+        }
+
+
+    println!("you typed: {}", input);
+    }
